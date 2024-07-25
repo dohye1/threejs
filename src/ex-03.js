6b975913732a1d964db585665e6bb396c62829fe
dohye1 <tsc03018@gmail.com>
2024-07-26 00:15:20 +0900

    03. 기본 뼈대 이해하기

--- src/ex-03.js
+++ src/ex-03.js
@@ -2,8 +2,13 @@ import * as THREE from 'three'
 import { WEBGL } from './webgl'
 
 if (WEBGL.isWebGLAvailable()) {
+  // 장면
   const scene = new THREE.Scene()
 
+  // scene의 배경색 설정
+  scene.background = new THREE.Color('tomato')
+
+  // 카메라
   const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -11,10 +16,12 @@ if (WEBGL.isWebGLAvailable()) {
     1000
   )
 
-  const renderer = new THREE.WebGLRenderer()
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  const canvas = document.getElementById('ex-03')
 
-  document.body.appendChild(renderer.domElement)
+  // 렌더러
+  const renderer = new THREE.WebGLRenderer({ canvas })
+
+  renderer.setSize(window.innerWidth, window.innerHeight)
   renderer.render(scene, camera)
 } else {
   var warning = WEBGL.getWebGLErrorMessage()
